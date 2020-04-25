36b3b93e42e68b021f48bc8e89a57194f68b26f1
alibaltschun <alibaltschun@gmail.com>
2020-04-25 18:44:48 +0800

    django gui attack movement armor

--- App/django/pages/views.py
+++ App/django/pages/views.py
@@ -29,7 +29,84 @@ def drafting(request):
             stat_meta(78),
             stat_win_rate(),
             stat_most_played(),
-        ]
+        ],
+        'summary': {
+            'attack': [
+                {
+                    'name': 'Zeus',
+                    'attack_min': 1,
+                    'attack_max': 2
+                },
+                {
+                    'name': 'Lina',
+                    'attack_min': 3,
+                    'attack_max': 4
+                },
+                {
+                    'name': 'Luna',
+                    'attack_min': 5,
+                    'attack_max': 6
+                },
+                {
+                    'name': 'Razor',
+                    'attack_min': 7,
+                    'attack_max': 8
+                },
+                {
+                    'name': 'Lion',
+                    'attack_min': 90,
+                    'attack_max': 10
+                }
+            ],
+            'armor': [
+                {
+                    'name': 'Zeus',
+                    'armor': 2
+                },
+                {
+                    'name': 'Lina',
+                    'armor': 4
+
+                },
+                {
+                    'name': 'Luna',
+                    'armor': 2
+
+                },
+                {
+                    'name': 'Razor',
+                    'armor': 3
+
+                },
+                {
+                    'name': 'Lion',
+                    'armor': 5
+
+                }
+            ],
+            'movement': [
+                {
+                    'name': 'Zeus',
+                    'movement_speed': 100,
+                },
+                {
+                    'name': 'Lina',
+                    'movement_speed': 100,
+                },
+                {
+                    'name': 'Luna',
+                    'movement_speed': 100,
+                },
+                {
+                    'name': 'Razor',
+                    'movement_speed': 100,
+                },
+                {
+                    'name': 'Lion',
+                    'movement_speed': 100,
+                }
+            ]
+        }
     }
 
     return render(request, template, propt)
