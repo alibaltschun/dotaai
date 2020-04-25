76e3a77f71d0055420c25417d1c7aed3ca16b9e2
alibaltschun <alibaltschun@gmail.com>
2020-04-25 15:45:15 +0800

    django drafting hero info index

--- App/django/pages/views.py
+++ App/django/pages/views.py
@@ -16,6 +16,245 @@ def drafting(request):
         'radiant_heros': [
 
             {
+                'index': 0,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 1,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 2,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 3,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 4,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            }
+        ],
+
+        'dire_heros': [
+
+            {
+                'index': 5,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 6,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 7,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 8,
+                'name': 'Zeus',
+                'strength': 25,
+                'agility': 14,
+                'intelligence': 16,
+                'attack_min': 65,
+                'attack_max': 71,
+                'armor': 0,
+                'movement': 280,
+
+                'work_well_with': [
+                    'Lion'
+                ],
+
+                'good_against': [
+                    'Lina',
+                    'Lion'
+                ],
+
+                'Bad_against': [
+                    'Lina',
+                    'Lion',
+                    'Crystal_Maiden'
+                ]
+            },
+            {
+                'index': 9,
                 'name': 'Zeus',
                 'strength': 25,
                 'agility': 14,
